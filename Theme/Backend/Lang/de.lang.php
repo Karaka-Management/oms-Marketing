f4e150cbd9808785c532a34c7f200d241b5a24aa
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Marketing' => [
+    'Actual' => '',
+    'Budget' => '',
+    'Description' => '',
+    'End' => '',
+    'Event' => '',
+    'Events' => '',
+    'Expenses' => '',
+    'Limit' => '',
+    'Location' => '',
+    'Name' => '',
+    'Promotion' => '',
+    'Promotions' => '',
+    'Sales' => '',
+    'Start' => '',
+    'Status' => '',
+    'Title' => '',
+    'Type' => '',
+    'Due' => '',
+]];
--- Theme/Backend/Lang/en.lang.php
+++ Theme/Backend/Lang/en.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\Marketing
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,21 +12,22 @@
 declare(strict_types=1);
 
 return ['Marketing' => [
-    'Actual'      => 'Actual',
-    'Budget'      => 'Budget',
+    'Actual' => 'Actual',
+    'Budget' => 'Budget',
     'Description' => 'Description',
-    'End'         => 'End',
-    'Event'       => 'Event',
-    'Events'      => 'Events',
-    'Expenses'    => 'Expenses',
-    'Limit'       => 'Limit',
-    'Location'    => 'Location',
-    'Name'        => 'Name',
-    'Promotion'   => 'Promotion',
-    'Promotions'  => 'Promotions',
-    'Sales'       => 'Sales',
-    'Start'       => 'Start',
-    'Status'      => 'Status',
-    'Title'       => 'Title',
-    'Type'        => 'Type',
+    'End' => 'End',
+    'Event' => 'Event',
+    'Events' => 'Events',
+    'Expenses' => 'Expenses',
+    'Limit' => 'Limit',
+    'Location' => 'Location',
+    'Name' => 'Name',
+    'Promotion' => 'Promotion',
+    'Promotions' => 'Promotions',
+    'Sales' => 'Sales',
+    'Start' => 'Start',
+    'Status' => 'Status',
+    'Title' => 'Title',
+    'Type' => 'Type',
+    'Due' => '',
 ]];
