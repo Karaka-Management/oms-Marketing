07ec3c1b6d4e16d5f2e0eec98591733a1c8e447a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-03-26 15:01:46 +0100

    undo serialize deprecation and switch to installExternal api calls

--- Models/PromotionAttribute.php
+++ Models/PromotionAttribute.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 
 namespace Modules\Marketing\Models;
 
-use phpOMS\Contract\ArrayableInterface;
 
 /**
  * Promotion class.
@@ -24,7 +23,7 @@ use phpOMS\Contract\ArrayableInterface;
  * @link    https://karaka.app
  * @since   1.0.0
  */
-class PromotionAttribute implements \JsonSerializable, ArrayableInterface
+class PromotionAttribute implements \JsonSerializable
 {
     /**
      * Id.
