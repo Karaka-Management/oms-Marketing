29c2a63bd146be91dc91b863786b6e56c7b7b980
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-12-25 00:06:17 +0100

    test fixes and changes for release

--- Models/Promotion.php
+++ Models/Promotion.php
@@ -374,6 +374,20 @@ class Promotion
     }
 
     /**
+     * Get relations to the promotions.
+     *
+     * This includes customers, partners, ...
+     *
+     * @return array
+     *
+     * @since 1.0.0
+     */
+    public function getRelations() : array
+    {
+        return $this->accountRelations;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function toArray() : array
