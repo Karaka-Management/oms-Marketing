9e3803698ed68a1ca1e7d4c31967ede56a9549cb
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 15:14:05 +0000

    fix tests

--- Models/PromotionAttributeValue.php
+++ Models/PromotionAttributeValue.php
@@ -111,7 +111,7 @@ class PromotionAttributeValue implements \JsonSerializable
     /**
      * Set value
      *
-     * @param int|string|float $value Value
+     * @param null|int|string|float $value Value
      *
      * @return void
      *
