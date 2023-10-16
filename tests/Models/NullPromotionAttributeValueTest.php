a0929b5ea57293270e667eefa03770e40908eb2b
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:39 +0000

    Add test cases for null models

--- tests/Models/NullPromotionAttributeValueTest.php
+++ tests/Models/NullPromotionAttributeValueTest.php
@@ -23,7 +23,7 @@ final class NullPromotionAttributeValueTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\Marketing\Models\NullPromotionAttributeValue
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullPromotionAttributeValueTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Marketing\Models\NullPromotionAttributeValue
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
@@ -42,7 +42,7 @@ final class NullPromotionAttributeValueTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Marketing\Models\NullPromotionAttributeValue
-     * @group framework
+     * @group module
      */
     public function testJsonSerialize() : void
     {
--- tests/Models/NullPromotionTest.php
+++ tests/Models/NullPromotionTest.php
@@ -23,7 +23,7 @@ final class NullPromotionTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\Marketing\Models\NullPromotion
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullPromotionTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Marketing\Models\NullPromotion
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
@@ -42,7 +42,7 @@ final class NullPromotionTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Marketing\Models\NullPromotion
-     * @group framework
+     * @group module
      */
     public function testJsonSerialize() : void
     {
