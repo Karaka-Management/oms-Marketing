75a05fe065a681e844aa8a71c033eb8e955b2dff
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullPromotionAttributeTest.php
+++ tests/Models/NullPromotionAttributeTest.php
@@ -47,6 +47,6 @@ final class NullPromotionAttributeTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullPromotionAttribute(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
