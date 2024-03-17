e49b72bfcd37b133b35536abcf707b05d79b2fc6
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-17 21:35:01 +0000

    fix tests

---
+++ Controller/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\Marketing
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Marketing\Controller;
+
+/**
+ * Marketing api controller class.
+ *
+ * @package Modules\Marketing
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+final class ApiController extends Controller
+{
+}
